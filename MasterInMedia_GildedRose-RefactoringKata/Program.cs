d50bc80b587868d5315d3e8261672222da945868
Diaa Elkhateeb <32547282+d2msk@users.noreply.github.com>
2019-07-27 14:43:55 +0200

    Add project files.

---
+++ MasterInMedia_GildedRose-RefactoringKata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterInMedia_GildedRose_RefactoringKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
