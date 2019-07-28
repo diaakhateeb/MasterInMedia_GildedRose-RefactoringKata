5fc0bbdf780bb42d32d5b709c0d852ca14c9d8eb
Diaa Elkhateeb <32547282+d2msk@users.noreply.github.com>
2019-07-28 21:38:27 +0200

    Add try-catch to UpdateQuality() and GetInstance() methods.

--- MasterInMedia_GildedRose-RefactoringKata/GildedRose.cs
+++ MasterInMedia_GildedRose-RefactoringKata/GildedRose.cs
@@ -1,4 +1,6 @@
 ﻿using MasterInMedia_GildedRose_RefactoringKata.Patterns.Factory;
+using NLog;
+using System;
 using System.Collections.Generic;
 
 namespace MasterInMedia_GildedRose_RefactoringKata
@@ -6,19 +8,36 @@ namespace MasterInMedia_GildedRose_RefactoringKata
     public class GildedRose<T> where T : Item
     {
         IList<T> _items;
+        private readonly Logger _logger;
         public GildedRose(IList<T> items)
         {
             _items = items;
+            _logger = LogManager.GetCurrentClassLogger();
         }
 
         public void UpdateQuality()
         {
-            var fac = new UpdateItemStrategyFactory<T>();
-
-            for (var i = 0; i < _items.Count; i++)
+            try
             {
-                var obj = fac.GetInstance(_items[i]);
-                obj?.UpdateItem();
+                var fac = new UpdateItemStrategyFactory<T>();
+
+                for (var i = 0; i < _items.Count; i++)
+                {
+                    var obj = fac.GetInstance(_items[i]);
+                    obj?.UpdateItem();
+                }
+            }
+            catch (TypeInitializationException typeInitExp)
+            {
+                _logger.Error(typeInitExp, typeInitExp.Message);
+            }
+            catch (TypeLoadException typeLoadExp)
+            {
+                _logger.Error(typeLoadExp, typeLoadExp.Message);
+            }
+            catch (Exception exp)
+            {
+                _logger.Error(exp, exp.Message);
             }
         }
     }
