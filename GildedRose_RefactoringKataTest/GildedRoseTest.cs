946019e746324c555c27acdbcb7957a7f15e03d5
Diaa Elkhateeb <32547282+d2msk@users.noreply.github.com>
2019-07-28 18:26:06 +0200

    Add StandardItem unit test.

--- GildedRose_RefactoringKataTest/GildedRoseTest.cs
+++ GildedRose_RefactoringKataTest/GildedRoseTest.cs
@@ -8,7 +8,7 @@ namespace GildedRose_RefactoringKataTest
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void StandardItem_UnitTest()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
@@ -16,6 +16,17 @@ namespace GildedRose_RefactoringKataTest
 
             Assert.AreEqual(-1, Items[0].SellIn);
             Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void StandardItem_QuantityEqual50_UnitTest()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.AreEqual(-1, Items[0].SellIn);
+            Assert.AreEqual(48, Items[0].Quality);
         }
     }
 }
